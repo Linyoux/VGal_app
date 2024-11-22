3a4f1e14f4cc149761a0ce44afcd9b7478d1f9eb
Linyoux <2635777403@qq.com>
2024-11-22 21:22:53 +0800

    Update LauncherActivity.java

--- app/src/main/java/client/vgal/LauncherActivity.java
+++ app/src/main/java/client/vgal/LauncherActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable; // 导入 BitmapDrawable
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
